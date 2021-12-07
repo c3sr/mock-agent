ae4f095b3afe6cfb72399ee78b1a56c873026783
Steve Zazeski <steve@pixotech.com>
2021-12-07 16:47:10 -0600

    Fixes for ci build

--- scripts/push-container.sh
+++ scripts/push-container.sh
@@ -1,3 +1,20 @@
 #!/usr/bin/env bash
 
+cd `git rev-parse --show-toplevel`
+source "scripts/functions.sh"
+
+USAGE="Usage: ${0} <version> (registry)
+    version       (required) The version number to tag the image with
+                      Note: image will always be tagged as 'latest'
+    registry      (optional) The Docker registry to tag images against
+                      Defaults to 'mock-agent'"
+
+VERSION="$1"
+REGISTRY="$2"
+: ${REGISTRY:="mock-agent"}
+
+if [ -z $VERSION ]; then
+    die_with_message "$USAGE"
+fi
+
 docker push "$REGISTRY/mock-agent:$VERSION"
